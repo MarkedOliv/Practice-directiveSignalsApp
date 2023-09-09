20d15dd3a6d59bf74ec515caaf5478502ec83c88
MarkedOliv <olivdev4@gmail.com>
2023-09-09 17:31:22 -0400

    Entendimiento de las mutaciones utilizando signals y computed, primeros pasos con effect

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
-  styleUrls: ['./properties-page.component.css']
+  styleUrls: ['./properties-page.component.css'],
 })
 export class PropertiesPageComponent {
+  public user = signal<User>({
+    id: 1,
+    email: 'george.bluth@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg',
+  });
+  onFieldUpdated(field: keyof User, value: string) {
+    // this.user.set({
+    //   ...this.user(),
+    //   [field]:value,
+    // });
 
+
+    this.user.mutate((current) => {
+      switch(field) {
+        case 'email':
+          current.email = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'id':
+          current.id = Number(value);
+          break;
+      }
+    })
+  }
 }
