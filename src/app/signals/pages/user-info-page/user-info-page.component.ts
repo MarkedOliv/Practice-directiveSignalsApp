f008689410549fef9a0f34f24b3cd4f443757947
MarkedOliv <olivdev4@gmail.com>
2023-09-09 17:05:32 -0400

    Practica de las signals y computed usando peticiones http

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import { UsersService } from '../../services/user-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './user-info-page.component.html',
-  styleUrls: ['./user-info-page.component.css']
+  styleUrls: ['./user-info-page.component.css'],
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+  private usersService = inject(UsersService);
+  public userId = signal(1);
 
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal<Boolean>(true);
+  public fullname = computed<string>(() => {
+    if (!this.currentUser()) return 'Usuario no encontrado';
+
+    return `${this.currentUser()?.first_name} ${this.currentUser()?.last_name}`;
+  });
+
+  loadUser(id: number): void {
+    if (id <= 0) return;
+
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+
+    this.usersService.getUserById(id).subscribe({
+      next: (user) => {
+        this.currentUser.set(user);
+        this.userWasFound.set(true);
+      },
+      error: () => {
+        this.userWasFound.set(false);
+        this.currentUser.set(undefined);
+      },
+    });
+  }
+  ngOnInit(): void {
+    this.loadUser(this.userId());
+  }
 }
